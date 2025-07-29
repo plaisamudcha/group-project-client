599bad35e820ed6769c1055cbe028de5d91b7baf
Thanaton2002 <thanaton.ims@gmail.com>
2025-07-29 15:34:29 +0700

    Fix Redirect path

--- src/pages/GoToHomePage.jsx
+++ src/pages/GoToHomePage.jsx
@@ -1,7 +1,16 @@
 import { Navigate } from "react-router";
+import useUserStore from "../stores/useUserStore.js";
 
-function GotoHomePage() {
-  return <Navigate to="/" replace />;
+function GotoHomePage({}) {
+  const user = useUserStore((state) => state.user);
+  const userRole = user?.role ; 
+  const finalPath =
+    userRole === "HR"
+      ? '/admin'
+      : userRole === "EMPLOYEE"
+      ? '/employee'
+      : '/';
+  return <Navigate to={finalPath} replace />;
 }
 
 export default GotoHomePage;
