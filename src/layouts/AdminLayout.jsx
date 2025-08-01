6c9c5f42e454522e0493aa1582677ce140b422b2
Thanaton2002 <thanaton.ims@gmail.com>
2025-08-01 11:14:52 +0700

    workpolicy 90 percent

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -7,7 +7,7 @@ function AdminLayout() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
+      <main className="w-full sticky">
       <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between">
         <SidebarTrigger />
         <UserNav />
